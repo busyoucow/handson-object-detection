a9109c2c2482975aecbc97d241e15cd183e64a3a
busyoucow <busyoucow@gmail.com>
2019-06-19 11:31:24 +0000

    Change frame rate01

--- sample.py
+++ sample.py
@@ -3,6 +3,8 @@ from imutils.video.pivideostream import PiVideoStream
 import cv2
 import time
 
+import numpy as np
+
 
 app = Flask(__name__)
 camera = PiVideoStream(resolution=(400, 304), framerate=8).start()
@@ -29,3 +31,53 @@ if __name__ == '__main__':
         threaded=True,
         use_reloader=False
     )
+
+net = cv2.dnn.readNetFromCaffe('/home/pi/models/MobileNetSSD_deploy.prototxt',
+        '/home/pi/models/MobileNetSSD_deploy.caffemodel')
+
+name: "MobileNet-SSD"
+input: "data"
+input_shape {
+  dim: 1
+  dim: 3
+  dim: 300
+  dim: 300
+}
+
+def detect(frame):
+    frame = cv2.resize(frame, (300, 300))
+    blob = cv2.dnn.blobFromImage(
+        image=frame,
+        scalefactor=0.007843,
+        size=(300, 300),
+        mean=127.5
+    )
+
+net.setInput(blob)
+out = net.forward()
+
+    boxes = out[0,0,:,3:7] * np.array([300, 300, 300, 300])
+    classes = out[0,0,:,1]
+    confidences = out[0,0,:,2]
+
+    for i, box in enumerate(boxes):
+        confidence = confidences[i]
+        if confidence < 0.2:
+            continue
+        idx = int(classes[i])
+        if idx != 15:
+            continue
+        (startX, startY, endX, endY) = box.astype('int')
+        cv2.rectangle(frame, (startX, startY), (endX, endY), (0, 255, 0), 2)
+        label = '{}: {:.2f}%'.format('Person', confidence * 100)
+        y = startY - 15 if startY - 15 > 15 else startY + 15
+        cv2.putText(frame, label, (startX, y), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 1)
+    return frame
+
+def gen(camera):
+    while True:
+        frame = camera.read()
+        processed_frame = detect(frame.copy())
+        ret, jpeg = cv2.imencode('.jpg', processed_frame)
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + jpeg.tobytes() + b'\r\n\r\n')
